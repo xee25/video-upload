d077cc2003fe5d5a255bee7d0e17aa337fa28c62
Zeeshan Farooq <xeeshan025@gmail.com>
2018-03-30 16:57:44 +0500

    comit

--- facebook/fb-callback.php
+++ facebook/fb-callback.php
@@ -2,10 +2,10 @@
 require_once '../vendor/autoload.php';
 
 $fb = new Facebook\Facebook([
-    'app_id' => '776131842592020',
-    'app_secret' => '1d769c3f565eebc391c54ec56bc79247',
+    'app_id' => '',
+    'app_secret' => '',
     'default_graph_version' => 'v2.2',
-    'default_access_token' => '2705b39ea13ccd0acd88cb9bf88d6bef',
+    'default_access_token' => '',
 //    'default_access_token' => 'EAALB40bGzRQBAJV5kZBgibR1ZCKMXi9yxtIqHVuZAW8154MHnebZCmA7p0heVohgDZCrPidI66bKfiEYVwqhwl65NzZAn6yUGxbbqCiZAkdU3TM3dcXgkZBJ0tqnD7Iia46Fe2zAVtD3XsLoZA4YePBc4JZBdVPGKLjDoR8XgY40DXAgD480L2BPLZAuzqW0MTHJFqS8TqwI6cCBuWxsAlP3ZCHUDD5n7mdseawLzhc2QFstlQZDZD',
 ]);
 
